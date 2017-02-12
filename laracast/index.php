655172a8bd0db1f7eed5d507dba08a0f3f01b6e7
Jean Ureña <juanpedro1519@gmail.com>
2017-02-12 15:41:32 -0300

    4- Separating Logic PHP and Views

--- laracast/index.php
+++ laracast/index.php
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<style type="text/css">
-		header
-		{
-			background: #e3e3e3;
-			padding: 2em;
-			text-align: center;
-		}
-	</style>
-</head>
-<body>
-	<header>
-		<h1>
-			<?php 
-				// $_GET -> Dictionary with the variables passed on get
-				// html specialChars to avoid code injections and take only string
-				echo "Hello " . htmlspecialchars($_GET["name"]);
-			?>
-		</h1>
-		<!-- shorthand  -->
-		<h2><?= "Your age is " . $_GET["age"] ?></h2>
-	</header>
-</body>
-</html>
+<?php
+	// separating concerns
+	$name = "Jean";
+	$age = 19;
+	$nationality = "Dominican";
+
+	// include the file
+	require "index.view.php";
+?>
---
+++ laracast/index.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		header
+		{
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Personal Data : </h1>
+		<p><b>Name : </b> <?= $name ?></p>
+		<p><b>Age : </b> <?= $age ?></p>
+		<p><b>Nationality : </b> <?= $nationality ?></p>
+	</header>
+</body>
+</html>
