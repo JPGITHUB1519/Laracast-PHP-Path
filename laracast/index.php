2844a28d510c2b80facf02fb433bb7d4c943ecc1
Jean Ureña <juanpedro1519@gmail.com>
2017-02-12 17:18:23 -0300

    Associative Arrays

--- laracast/index.php
+++ laracast/index.php
@@ -1,17 +1,24 @@
 <?php
-	// separating concerns
-	$names = [
-		"Jean",
-		"Isaias",
-		"Emiliano",
-		"Felipe",
+	// associative array
+	$person = [
+		"name" => "jean",
+		"age" => "19",
+		"career" => "Scrum Team Member"
 	];
 
-	$animals = [
-		"cat",
-		"dog",
-		"lion"
-	];
+	// add key / value to dictionary
+	$person["birthdate"] = 1998; 
 
+	// delete from array
+	unset($person["age"]);
+
+	// print array on screen
+	var_dump($person);
+	echo "<br>";
+	// show formatted
+	print_r($person);
+
+	// die print the message and exit the script
+	die("Print and exit the Script");
 	require "index.view.php";
 ?>
--- laracast/index.view.php
+++ laracast/index.view.php
@@ -13,26 +13,11 @@
 </head>
 <body>
 	<header>
-		<!-- dirty way -->
-		<!-- <ul>
-			<?php
-				/*
-				foreach($names as $value)
-				{
-					echo "<li>$value</li>";
-				}
-				*/
-			?>
-		</ul> -->
-		<h1>Friends</h1>
-		<?php foreach($names as $name): ?>
-			<li><?= $name ?></li>
+		<h1>Personal Data </h1>
+		<?php foreach($person as $key => $feature) : ?>
+			<li><strong><?= $key ?></strong><?= $feature ?></li>
 		<?php endforeach; ?>
 
-		<h1>Favorite Animals</h1>
-		<?php foreach($animals as $animal): ?>
-			<li><?php echo $animal ?></li>
-		<?php endforeach; ?>
 	</header>
 </body>
 </html>
