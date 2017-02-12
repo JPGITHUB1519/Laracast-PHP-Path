7fc75f727a70252d50e28e559b96e1e581b776d6
Jean Ureña <juanpedro1519@gmail.com>
2017-02-12 15:28:48 -0300

    3 - PHP EMBBEDED WITH HTML AND GET VARIABLE

--- laracast/index.php
+++ laracast/index.php
@@ -1,12 +1,27 @@
-<?php
-	// run php
-	// php -S localhost:port
-	$name = "Jean";
-	$age = 19;
-
-	echo 'Hello $name <br>';
-	// with double quotes we can see variables
-	echo "Hello {$name} <br>"; // same echo "Hello $name";
-	// concatenation
-	echo "Hello " . $name . " You Have " . $age . " Years Old";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		header
+		{
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>
+			<?php 
+				// $_GET -> Dictionary with the variables passed on get
+				// html specialChars to avoid code injections and take only string
+				echo "Hello " . htmlspecialchars($_GET["name"]);
+			?>
+		</h1>
+		<!-- shorthand  -->
+		<h2><?= "Your age is " . $_GET["age"] ?></h2>
+	</header>
+</body>
+</html>
