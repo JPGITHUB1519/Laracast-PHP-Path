d36bd49eaf9bbced6c829c8f0c570c26c5c33ce3
Jean Ureña <juanpedro1519@gmail.com>
2017-03-04 15:41:49 -0300

    Intro To PDO

--- laracast/functions.php
+++ laracast/functions.php
@@ -19,4 +19,25 @@
 			return False;
 		}
 	}
+
+	function connectToDb()
+	{
+		try
+		{
+			return $pdo = new PDO('mysql:host=localhost;dbname=mytodo', 'root', 'root');
+		}
+		catch(PDOException $e)
+		{
+			die('Could Not Connect to Database');
+		}
+	}
+
+	function fetchAllTasks($pdo)
+	{
+		$statement = $pdo->prepare('SELECT * FROM todos');
+		$statement->execute();
+		// TO CONVERT TO CUSTOM CLASSES
+		$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+		return $tasks;
+	}
 ?>
--- laracast/index.php
+++ laracast/index.php
@@ -1,42 +1,11 @@
 <?php
+	require 'Task.php';
 	require 'functions.php';
-	class Task
-	{
-		// attributes
-		public $description;
-		public $completed = False;
 
-		// constructor
-		public function __construct($description)
-		{
-			$this->description = $description;
-		}
-
-		public function isCompleted()
-		{
-			return $this->completed;
-		}
-
-		public function complete()
-		{
-			$this->completed = True;
-		}
-
-	}
-
-	// $task = new Task("Do My Homework");
-	// $task->complete();
-	// var_dump($task->iscompleted());
-
-	// arrays of tasks
-	$tasks = [
-		new Task("Go to Store"),
-		new Task("Play Video Games"),
-		new Task("Do my Homework")
-	];
-
-	$tasks[0]->complete();
-
+	
+	$pdo = connectToDb();
+	$tasks = fetchAllTasks($pdo);
+	var_dump($tasks[0]->foo());
 	require "index.view.php";
 
 ?>
