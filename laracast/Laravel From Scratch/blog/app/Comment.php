6901118de8b49c221dbef455569eaea221e8f228
Jean Ureña <juanpedro1519@gmail.com>
2017-03-20 23:50:51 -0400

    View Composers

--- laracast/Laravel From Scratch/blog/app/Comment.php
+++ laracast/Laravel From Scratch/blog/app/Comment.php
@@ -20,3 +20,4 @@ class Comment extends Model
     {
     	return $this->belongsTo(User::class);
     }
+}
--- laracast/Laravel From Scratch/blog/app/Providers/AppServiceProvider.php
+++ laracast/Laravel From Scratch/blog/app/Providers/AppServiceProvider.php
@@ -14,6 +14,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        // view composer -> show this variable in all views
+        // we can create a class for this if we want
+        // is a callback that is called when a view is rendering
+        // name of the view loading
+        view()->composer('layouts.sidebar', function($view){
+            $view->with('archives', \App\Post::archives());
+        });
     }
 
     /**
