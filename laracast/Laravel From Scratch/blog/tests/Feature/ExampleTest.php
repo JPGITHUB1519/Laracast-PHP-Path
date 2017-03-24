468b1cc4e4a3288387bbaf774761a9f13189336b
Jean Ureña <juanpedro1519@gmail.com>
2017-03-23 23:03:25 -0400

    Testing 101

--- laracast/Laravel From Scratch/blog/tests/Feature/ExampleTest.php
+++ laracast/Laravel From Scratch/blog/tests/Feature/ExampleTest.php
@@ -3,12 +3,15 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Post;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    // for rollback 
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -16,8 +19,30 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        // $response = $this->get('/')->assertSee('The Bootstrap Blog');
 
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
+
+        // Given I have to record in the database that are posts, 
+        // and each one is posted a month apart
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+                'created_at' => \Carbon\Carbon::now()->subMonth()
+            ]);
+        // When i fetch the archives
+        $posts = Post::archives();
+        // Then the response should be in the proper format
+        $this->assertEquals([
+                [
+                    "year" => $first->created_at->format('Y'),
+                    "month" => $first->created_at->format('F'),
+                    "published" => 1
+                ],
+                [
+                    "year" => $second->created_at->format('Y'),
+                    "month" => $second->created_at->format('F'),
+                    "published" => 1
+                ],
+            ], $posts);
     }
 }
